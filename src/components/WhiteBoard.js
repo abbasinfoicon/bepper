94b618b929134b92c73a15a1bb00a5722473e51a
uid3 <uid3@infoiconsoftware.com>
2024-08-13 17:52:26 +0530

    live

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -1,13 +1,18 @@
 'use client';
+import { LiveCollaborationTrigger } from '@excalidraw/excalidraw';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Dynamically import Excalidraw with SSR disabled
-const Excalidraw = dynamic(() => import('@excalidraw/excalidraw').then((mod) => mod.Excalidraw), {
+const Excalidraw = dynamic(() => import('@excalidraw/excalidraw').then(mod => mod.Excalidraw), {
     ssr: false,
+    loading: () => <div className="juNRvt"><span className="loader"> </span></div>,
 });
 
 const WhiteBoard = () => {
+    const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+    const [isCollaborating, setIsCollaborating] = useState(false);
+
     const UIOptions = {
         canvasActions: {
             loadScene: false,
@@ -18,9 +23,53 @@ const WhiteBoard = () => {
         },
     };
 
+    useEffect(() => {
+        if (excalidrawAPI) {
+            if (isCollaborating) {
+                const collaborators = new Map();
+                collaborators.set("id1", {
+                    username: "Doremon",
+                    avatarUrl: "../../../../img/doremon.png",
+                });
+                collaborators.set("id3", {
+                    username: "Pika",
+                    avatarUrl: "../../../../img/pika.jpeg",
+                });
+                excalidrawAPI.updateScene({ collaborators });
+            } else {
+                excalidrawAPI.updateScene({
+                    collaborators: new Map(),
+                });
+            }
+        }
+    }, [isCollaborating, excalidrawAPI]);
+
     return (
         <div style={{ height: 'calc(100vh - 50px)' }}>
-            <Excalidraw UIOptions={UIOptions} />
+            <label style={{ fontSize: "16px", fontWeight: "bold" }}>
+                <input
+                    type="checkbox"
+                    checked={isCollaborating}
+                    onChange={() => setIsCollaborating(!isCollaborating)}
+                />
+                Show Collaborators
+            </label>
+
+            <Excalidraw
+                onChange={(elements, state, appState) => {
+                    if (!excalidrawAPI) setExcalidrawAPI(state.api);
+                }}
+                UIOptions={UIOptions}
+                renderTopRightUI={() => (
+                    <LiveCollaborationTrigger
+                        isCollaborating={isCollaborating}
+                        onSelect={() => {
+                            window.alert("You clicked on collab button");
+                            setIsCollaborating(true);
+                        }}
+                    />
+                )}
+            />
         </div>
     );
 };
